2d541858b18dcd8209bd74f91331f1e1f384b6a4
NicolaSforza93 <nicola.sforza93@gmail.com>
2023-11-21 16:52:52 +0100

    added task

---
+++ form.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP Badwords</title>
+</head>
+
+<body>
+    <form action="index.php" method="POST">
+        <input type="text" name="replace_word">
+        <input type="submit" value="Invia">
+    </form>
+</body>
+
+</html>
--- index.php
+++ index.php
@@ -1,11 +1,36 @@
+<?php
+$text = "Con l'aiuto di fedeli robot e altri preziosi alleati, il giovane Luke Skywalker deve salvare la principessa ribelle Leila e sconfiggere l'Impero che costringe la galassia sotto il suo controllo.";
+$replace = $_POST['replace_word'];
+
+?>
+
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>PHP Badwords</title>
+    <title>Document</title>
 </head>
+
 <body>
-    
+    <!-- Stampo il testo -->
+    <p><?php echo $text; ?></p>
+    <!-- Stampo la lunghezza del testo -->
+    <p>Il testo è lungo <?php echo strlen($text); ?> caratteri</p>
+    <!-- Salvo il testo censurato in una variabile -->
+    <p><?php $censored = str_replace($replace, '***', $text); ?></p>
+    <!-- Stampo il testo censurato -->
+    <p><?php echo str_replace($replace, '***', $text); ?></p>
+    <!-- Stampo la lunghezza del testo censurato -->
+    <p>Il testo è lungo <?php echo strlen($censored); ?> caratteri</p>
+
+    <?php
+
+    // var_dump($text);
+
+    ?>
+
 </body>
+
 </html>
